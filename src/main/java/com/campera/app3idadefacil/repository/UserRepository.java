ecf81417e71d14b15b54ffc4d713d4f1e66446c4
Bruno Campera <brunocmpr@gmail.com>
2023-03-02 21:06:25 -0300

    Create UserDetailsService implementation

---
+++ src/main/java/com/campera/app3idadefacil/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.campera.app3idadefacil.repository;
+
+import com.campera.app3idadefacil.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByEmail(String email);
+}
