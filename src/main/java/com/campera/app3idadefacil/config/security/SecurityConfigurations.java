77a81a5ea9cec8ef47076a187930df74d835feec
Bruno Campera <brunocmpr@gmail.com>
2023-03-02 22:08:41 -0300

    Configure user detail service for authentication

--- src/main/java/com/campera/app3idadefacil/config/security/SecurityConfigurations.java
+++ src/main/java/com/campera/app3idadefacil/config/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package com.campera.app3idadefacil.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,13 +8,19 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AuthService authService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(authService)
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Override
