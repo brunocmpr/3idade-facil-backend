95151e30169405497963f8898a83fbf1392e8c44
Bruno Campera <brunocmpr@gmail.com>
2023-04-01 21:16:49 -0300

    Improve bean validation

--- src/main/java/com/campera/app3idadefacil/controller/DrugController.java
+++ src/main/java/com/campera/app3idadefacil/controller/DrugController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -33,7 +34,7 @@ public class DrugController {
 
     @PostMapping
     @Operation(summary = "Register new drug")
-    public ResponseEntity<DrugDto> createDrug(@RequestBody DrugForm drugForm, Authentication authentication){
+    public ResponseEntity<DrugDto> createDrug(@Valid @RequestBody DrugForm drugForm, Authentication authentication){
         AppUser appUser = (AppUser) authentication.getPrincipal();
         Drug drug = service.createDrug(drugForm, appUser);
         DrugDto drugDto = new DrugDto(drug);
--- src/main/java/com/campera/app3idadefacil/model/datatransfer/form/DrugForm.java
+++ src/main/java/com/campera/app3idadefacil/model/datatransfer/form/DrugForm.java
@@ -4,7 +4,10 @@ import com.campera.app3idadefacil.model.AppUser;
 import com.campera.app3idadefacil.model.Drug;
 import lombok.Data;
 
+import javax.validation.constraints.Size;
+
 @Data
 public class DrugForm {
+    @Size(min = 2, max= 150)
     private String name;
 }
