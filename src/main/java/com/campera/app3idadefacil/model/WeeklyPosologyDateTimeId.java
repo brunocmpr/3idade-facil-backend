f8f4e49ff238c39d8e872617a13758ce7c4a6f14
Bruno Campera <brunocmpr@gmail.com>
2023-04-07 17:25:38 -0300

    Improve WeeklyPosologyDateTime Id mapping

--- src/main/java/com/campera/app3idadefacil/model/WeeklyPosologyDateTimeId.java
+++ src/main/java/com/campera/app3idadefacil/model/WeeklyPosologyDateTimeId.java
@@ -1,14 +1,12 @@
 package com.campera.app3idadefacil.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.DayOfWeek;
 
-@Embeddable @Data
+@Data @AllArgsConstructor @NoArgsConstructor
 public class WeeklyPosologyDateTimeId implements Serializable {
     @Column(name = "plan_id", nullable = false, updatable = false) @Getter @Setter
     private Long planId;
