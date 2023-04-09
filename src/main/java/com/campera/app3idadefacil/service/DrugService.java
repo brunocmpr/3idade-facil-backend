c2c0e0ba37b2b4221365a38cbbde0ac4d3a32b78
Bruno Campera <brunocmpr@gmail.com>
2023-04-08 23:15:01 -0300

    Implement saving pictures of medication

--- src/main/java/com/campera/app3idadefacil/service/DrugService.java
+++ src/main/java/com/campera/app3idadefacil/service/DrugService.java
@@ -3,30 +3,54 @@ package com.campera.app3idadefacil.service;
 import com.campera.app3idadefacil.exception.ExistingEntityException;
 import com.campera.app3idadefacil.model.AppUser;
 import com.campera.app3idadefacil.model.Drug;
+import com.campera.app3idadefacil.model.Image;
 import com.campera.app3idadefacil.model.datatransfer.mapper.DrugMapper;
 import com.campera.app3idadefacil.model.datatransfer.form.DrugForm;
 import com.campera.app3idadefacil.repository.DrugRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class DrugService {
     @Autowired
     private DrugRepository repository;
 
+    @Autowired
+    private ImageService imageService;
+
     public List<Drug> findAllByMainCaretaker(AppUser appUser){
         return repository.findByCaretaker(appUser);
     }
 
-    public Drug createDrug(DrugForm drugForm, AppUser appUser) {
-        if(!repository.findByNameIgnoreCaseAndCaretaker(drugForm.getName(), appUser).isEmpty()){
+    @Transactional
+    public Drug createDrug(DrugForm drugForm, AppUser appUser, List<MultipartFile> multipartFiles) {
+        guardClausesCreateDrug(drugForm, multipartFiles, appUser);
+        Drug drug = DrugMapper.fromForm(drugForm, appUser);
+        List<UUID> uuids = imageService.saveFilesCreateUuids(multipartFiles);
+        List<Image> images = uuids.stream().map(UUID::toString).map(Image::new).collect(Collectors.toList());
+        drug.setImages(images);
+        Drug savedDrug = repository.save(drug);
+        images.forEach(image -> image.setDrug(savedDrug));
+        List<Image> savedImages = imageService.saveAll(images);
+
+        savedDrug.setImages(savedImages);
+        return savedDrug;
+    }
+
+    private void guardClausesCreateDrug(DrugForm drugForm, List<MultipartFile> files, AppUser appUser) {
+        if(!repository.findByNameIgnoreCaseAndStrengthIgnoreCaseAndCaretaker(drugForm.getName(), drugForm.getStrength()
+                , appUser).isEmpty()){
             throw new ExistingEntityException("Esta medicação já foi registrada");
         }
-        Drug drug = DrugMapper.fromForm(drugForm, appUser);
-        return repository.save(drug);
+
+        imageService.guardClausesCreateImages(files);
     }
 
     public Optional<Drug> findById(Long drugId) {
