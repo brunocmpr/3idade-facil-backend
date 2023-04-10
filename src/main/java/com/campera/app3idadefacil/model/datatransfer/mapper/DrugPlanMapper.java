58a08a4ab3aedddee52d9a32c54351fb710372f8
Bruno Campera <brunocmpr@gmail.com>
2023-04-09 22:30:47 -0300

    Clears possible multiple posologies from same drug plan

--- src/main/java/com/campera/app3idadefacil/model/datatransfer/mapper/DrugPlanMapper.java
+++ src/main/java/com/campera/app3idadefacil/model/datatransfer/mapper/DrugPlanMapper.java
@@ -16,17 +16,23 @@ public class DrugPlanMapper {
             case UNIFORM -> {
                 drugPlan.setUniformPosology(new UniformPosology(planForm.getUniformPosology()));
                 drugPlan.getUniformPosology().setDrugPlan(drugPlan);
+                drugPlan.setCustomPosologies(null);
+                drugPlan.setWeeklyPosology(null);
                 break;
             }
             case WEEKLY -> {
                 drugPlan.setWeeklyPosology(new WeeklyPosology(planForm.getWeeklyPosology()));
                 drugPlan.getWeeklyPosology().setDrugPlan(drugPlan);
+                drugPlan.setUniformPosology(null);
+                drugPlan.setCustomPosologies(null);
                 break;
             }
             case CUSTOM -> {
                 drugPlan.setCustomPosologies(planForm.getCustomPosologies().stream().map(CustomPosology::new)
                         .collect(Collectors.toList()));
                 drugPlan.getCustomPosologies().stream().forEach(posology -> posology.setDrugPlan(drugPlan));
+                drugPlan.setUniformPosology(null);
+                drugPlan.setWeeklyPosology(null);
                 break;
             }
         }
