dc5a948f3872f875f20d197e44b66a799092e6a1
Bruno Campera <brunocmpr@gmail.com>
2023-04-24 19:40:11 -0300

    Include entity id in posology DTOs

--- src/main/java/com/campera/app3idadefacil/model/datatransfer/dto/CustomPosologyDto.java
+++ src/main/java/com/campera/app3idadefacil/model/datatransfer/dto/CustomPosologyDto.java
@@ -8,9 +8,11 @@ import java.time.LocalDateTime;
 
 @Data
 public class CustomPosologyDto {
+    private Long id;
     private LocalDateTime dateTime;
 
     public  CustomPosologyDto(CustomPosology customPosology){
+        this.id = customPosology.getId();
         this.dateTime = customPosology.getDateTime();
     }
 }
